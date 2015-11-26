45946d8cdc1dde4c458c450acdfe02ff81b3197a
simo14 <s.morenou@alumnos.urjc.es>
2015-11-26 20:10:48 +0100

    N procesos unidos por pipe en prueba

--- prueba.c
+++ prueba.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
+#include <fcntl.h>
+
 int main () {
 	int i;
-	int n = 5;	//5 procesos
-	int pid[6];
+	int n = 3;	//Number of processes
+	int pid[n];
 
-	int pipeta[n][2];
+	int pipeta[n-1][2];
 	//Hacer los pipes
 	for(i=0; i<n-1; i++){
 		pipe(pipeta[i]);
@@ -14,13 +20,30 @@ int main () {
 	for(i = 0; i<n; i++){
 		pid[i] = fork();
 		if(pid[i]==0){
-			if((i<n-1)&&(i>0)){
-				dup2(pipeta[0][i-1],0);
-				dup2(pipeta[0][i+1],1);
+			if((i<n)&&(i>0)){				//Problema: hace un pipe cada vez. en i=0 hace el primer pipe y exec. la salida del pipe ni siquiera está asignada
+				dup2(pipeta[i-1][0],0);
+				printf("id %d comes from pipeta %d",i,i-1);
+				dup2(pipeta[i][1],1);
+			}else if(i==0){
+				dup2(pipeta[i][1],1);
+			}else if(i==n-1){
+				dup2(pipeta[i-1][0],0);
 			}
-				printf("soy el hijo %d",i);
-				exit(0);
+			/*if(i==0){
+				execlp("ls","ls", NULL);
+			}
+			else{
+				execlp("grep","grep","p", NULL);
+			}
+			printf("soy el hijo %d",i);
+			exit(0);*/
 		}
+	}
+	if(pid[0]==0){	//hijo original
+		execlp("ls","ls", NULL);
+	}
+	else if (pid[n-1]==0){
+		execlp("grep","grep","p", NULL);
 	}
 	if(pid[2]==0){
 		puts("la hemos cagado, los hijos ejecutan esta mierda");
