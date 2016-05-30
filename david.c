7dbcfc483498a481a6e2c0cb473835ef2cd04a9f
Jaldir <dangel.leo0@gmail.com>
2016-05-30 21:46:40 +0200

    Signal handling supported

--- david.c
+++ david.c
@@ -7,15 +7,37 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <signal.h>
 #include "parser.h"
 
+//Constants
 static const char PROMPT[] = "msh>>";
- 
+
+//Global vars
+int fgPID;
+
+//Signal handlers
+
+void sigintHandler (int sig) {
+	if (getpid() == fgPID) {
+		exit(EXIT_FAILURE);
+	}
+}
+void sigquitHandler (int sig) {
+	if (getpid() == fgPID) {
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main() {
 	//Init vars
 	char input[1024];
 	tline *tokens;
 	int exit = 0;
+
+	//signal(SIGINT, sigintHandler);
+	signal(SIGQUIT, sigquitHandler);
+
 	do{
 		printf(PROMPT);
 		if(fgets(input, sizeof(input), stdin)){
@@ -24,7 +46,6 @@ int main() {
 		if((tokens->commands==NULL)||(tokens==NULL)){  //Tokenize fault. If not checked it will lead to segmentation fault in the case of empty redirect		
 		} else {
 			executeLine (tokens);
-			wait(NULL);
 		}
 	} while (!exit); //Ask for inputs until exit = true
 }
@@ -51,7 +72,7 @@ int executeLine(tline *line){
 	//check output redirection
 	if (line->redirect_output == NULL){
 	}else{
-		int file = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC);
+		int file = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 		if(file < 0) {
 			fprintf(stderr,"Output redirect error: File not found");
 			return 1;
@@ -62,7 +83,7 @@ int executeLine(tline *line){
 	//check error output redirection
 	if (line->redirect_error == NULL){
 	}else{
-		int file = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC);
+		int file = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 		if(file < 0) if(file < 0) {
 			fprintf(stderr,"Error output redirect error: File not found");
 			return 1;
@@ -72,13 +93,13 @@ int executeLine(tline *line){
 
 	//Check if there're pipes
 	if (line->ncommands>1){
-		forkPipes(stdinRedir, stdoutRedir, stdErrorRedir, line->ncommands, line->commands);
-		waitpid(-1, NULL, 0);
+		fgPID = forkPipes(stdinRedir, stdoutRedir, stdErrorRedir, line->ncommands, line->commands);
 	} else {
-		forkPipes(stdinRedir, stdoutRedir, stdErrorRedir, line->ncommands, line->commands);
-		waitpid(-1, NULL, 0);
+		fgPID = forkPipes(stdinRedir, stdoutRedir, stdErrorRedir, line->ncommands, line->commands);
+		
 	}
-
+	waitpid(fgPID, &status, 0);
+	fgPID = 0;
 
 	//Close and return
 	if (stdinRedir != 0) close (stdinRedir);
@@ -90,7 +111,7 @@ int executeLine(tline *line){
 
 int forkPipes (int inFD, int outFD, int errFD, int n, tcommand *commands){	
 	int i, status;
-	pid_t pid;
+	int pid;
 	int in, out, fd [2];
 	// First command uses the original FD
 	in = inFD;
@@ -106,14 +127,13 @@ int forkPipes (int inFD, int outFD, int errFD, int n, tcommand *commands){
 		in = fd [0];
 	}
 
-	//Execute the last one
-	spawnProc (in, outFD, errFD, (commands+n)-1);
-	return 0;
+	//Execute the last one and return it's PID
+	return spawnProc (in, outFD, errFD, (commands+n)-1);
 }
 
 
 int spawnProc (int in, int out, int err, tcommand *command){
-	pid_t pid;
+	int pid;
 	int status;
 	if ((pid = fork ()) == 0){
 		if (in != 0){
@@ -137,3 +157,5 @@ int spawnProc (int in, int out, int err, tcommand *command){
 	}
 	return pid;
 }
+
+
