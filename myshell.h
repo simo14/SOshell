c5b7d7ba22b420f5163ce76b136dc89165b900b6
simo14 <s.morenou@alumnos.urjc.es>
2016-06-11 13:49:36 +0200

    .h modified

--- myshell.h
+++ myshell.h
@@ -3,6 +3,7 @@ int* forkPipes(int inFD, int outFD, int errFD, int n, tcommand *commands);
 int getSingleTextStatus(char * text, int pid);
 int getTextStatus(char * text, struct tsequence *sequence);
 int jobs(struct tprocessList * list);
+int toFG (int toFG);	//fg
 void sigintHandler(int sig);
 void sigquitHandler(int sig);
 void sigtstpHandler(int sig);
